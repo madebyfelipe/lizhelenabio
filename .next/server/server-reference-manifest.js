918d64e0fc77db72ecea36e00f34d3f9dc6e559d
madebyfelipe <byonichip@gmail.com>
2026-04-25 19:51:23 -0300

    qqqq

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"DcPvIw5E36eCAA6drODfVLgzVZolA/KwBhQDwCNcSAU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"NCjDx2y3QK8VUhYd5P79/f3Va9ZqzpzJqs8ML4GVtfg=\"\n}"
